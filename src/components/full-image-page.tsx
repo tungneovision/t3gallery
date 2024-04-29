d66fc581ca2f2f47b9c45d1330bf372733cd0571
Alexandru Posirca <alexandru.posirca@neovision.dev>
2024-04-29 17:22:27 +0300

    parallel routes

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,15 +1,26 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
   return (
     <div className="flex h-full w-full">
       <div className=" flex flex-shrink items-center justify-center">
         <img src={image.url} className=" flex-shrink object-contain" />
       </div>
 
-      <div className="flex-shirnk-0 border-l flex w-48 flex-col">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex-shirnk-0 flex w-48 flex-col gap-2 border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+        <div className="flex flex-col p-2">
+          <span>upload by : </span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>created on : </span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
